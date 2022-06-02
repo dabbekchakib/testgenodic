e5e0dda42ef31bd92d2b5fd79ae1d7a71992c661
dabbekchakib <dabbekchakib@gmail.com>
2022-06-02 13:34:55 +0100

    edit field type user and article

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -12,14 +19,30 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            
+            TextField::new('titre'),
             TextEditorField::new('description'),
+            ImageField::new('fichier')
+            ->setBasePath('uploads/articles/')
+            ->setUploadDir('public/uploads/articles/')
+            ->setFormType(FileUploadType::class)
+            ->setUploadedFileNamePattern('[randomhash].[extension]'),
+            //TextField::new('fichier')->setFormType(FileType::class),
+            DateField::new('createdAt'),
+            TextField::new('auteur')
         ];
     }
-    */
+  /*   public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('titre')
+            ->add('auteur')
+        ;
+    } */
+    
+    
 }
