84eea4510a022e0d42589bdddde64f762b6952f5
dabbekchakib <dabbekchakib@gmail.com>
2022-06-18 00:44:52 +0100

    add video, fix hashtag dshboard, error 500 page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
