f20ee0f90ade1f6eab8fe720b4fb365f5942cb0b
Halima2014 <benkhalifahalima@hotmail.com>
2022-09-19 19:26:29 +0200

    modification_home.html.twig

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,6 +29,7 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'titre' => 'Connexion']);
+        
     }
 
     /**
