86ad34217da12c631c4829ec217901b8e16ef176
dabbekchakib <dabbekchakib@gmail.com>
2022-09-22 21:08:40 +0100

    redirect route after auth admin/user

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -52,7 +52,10 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-
+        $user= $token->getUser();
+        if (!in_array('ROLE_ADMIN', $user->getRoles())) {
+            return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        }
         return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
