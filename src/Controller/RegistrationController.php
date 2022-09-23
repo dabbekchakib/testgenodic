8d3040050212704f142ef17653b62720308900f5
dabbekchakib <dabbekchakib@gmail.com>
2022-09-23 11:41:00 +0100

    ajout de verify email bundle

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,9 +14,21 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
+    private $verifyEmailHelper;
+    private $mailer;
+    
+    public function __construct(VerifyEmailHelperInterface $helper, MailerInterface $mailer)
+    {
+        $this->verifyEmailHelper = $helper;
+        $this->mailer = $mailer;
+    }
     /**
      * @Route("/register", name="app_register")
      */
@@ -27,6 +39,7 @@ class RegistrationController extends AbstractController
        
         $user = new User();
         
+
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
         
@@ -61,6 +74,25 @@ class RegistrationController extends AbstractController
             //     $request
             // );
             
+
+         // handle the user registration form and persist the new user...
+    
+         $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'registration_confirmation_route',
+            $user->getId(),
+            $user->getEmail()
+        );
+    
+    $email = new TemplatedEmail();
+    $email->from('send@example.com');
+    $email->to($user->getEmail());
+    $email->htmlTemplate('registration/confirmation_email.html.twig');
+    $email->context(['signedUrl' => $signatureComponents->getSignedUrl()]);
+    
+    $this->mailer->send($email);
+
+    // generate and return a response for the browser
+
             $this->addFlash('success', 'Votre inscription est validée. Connectez-vous à présent !');
             return $this->redirectToRoute('app_login');
         }
@@ -69,5 +101,28 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
             'titre' => 'Inscription'
         ]);
+    }
+        /**
+     * @Route("/verify", name="registration_confirmation_route")
+     */
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+
+        // Do not get the User's Id or Email Address from the Request object
+        try {
+            $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('verify_email_error', $e->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        // Mark your user as verified. e.g. switch a User::verified property to true
+
+        $this->addFlash('success', 'Your e-mail address has been verified.');
+
+        return $this->redirectToRoute('app_home');
     }
 }
